0eb1944a8e347645888bd418b253e647cceda7dd
defskela <dafskela@gmail.com>
2024-05-15 13:39:34 +0300

    логика страницы профиля пользователя

--- src/pages/profile/ui/index.tsx
+++ src/pages/profile/ui/index.tsx
@@ -1,4 +1,5 @@
 import cn from 'classnames'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ProfileCard } from '@/entities/profile-card'
 import { useGetUsers } from '@/entities/reviews-card/api'
@@ -16,7 +17,43 @@ export const Profile = () => {
         isLoading: isLoadingUser,
     } = useGetUsers(Number(id))
 
-    console.log(user)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [username, setName] = useState('')
+    const [editEmail, setEditEmail] = useState(false)
+    const [editPassword, setEditPassword] = useState(false)
+
+    const [defaultEmail, setDefaultEmail] = useState('')
+    const [defaultPassword, setDefaultPassword] = useState('')
+
+    useEffect(() => {
+        if (user) {
+            setEmail(user.email)
+            setName(user.username)
+            setDefaultEmail(user.email)
+            setDefaultPassword('')
+        }
+    }, [user])
+
+    const emailEditHandler = () => {
+        setEditEmail(!editEmail)
+        if (editEmail) {
+            setEmail(defaultEmail)
+        }
+    }
+    const passwordEditHandler = () => {
+        setEditPassword(!editPassword)
+        if (editPassword) {
+            setPassword(defaultPassword)
+        }
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const formObject = Object.fromEntries(formData.entries())
+        console.log(formObject)
+    }
 
     return (
         <div className={styles.profile}>
@@ -25,50 +62,80 @@ export const Profile = () => {
             ) : isErrorUser ? (
                 <p>Ошибка</p>
             ) : (
-                <ProfileCard
-                    className={styles.profile__info}
-                    username={user?.username || ''}
-                    image={user?.avatar || ''}
-                    stars={user?.rating || 0}
-                />
+                <div className={styles.left_block}>
+                    <ProfileCard
+                        className={styles.profile__info}
+                        username={user?.username || ''}
+                        image={user?.avatar || ''}
+                        stars={user?.rating || 0}
+                    />
+                    <Button className={styles.close_profile}>ЗАКРЫТЬ</Button>
+                </div>
             )}
             <div className={styles.wrapper}>
                 <div className={styles.block}>
                     <p className={styles.active}>Настройки</p>
                 </div>
-                <form
-                    onSubmit={(e) => {
-                        e.preventDefault()
-                    }}
-                >
+                <form onSubmit={handleSubmit}>
                     <div className={styles.inputs}>
                         <div className={styles.input}>
                             <label htmlFor="profile__email">Ваш email</label>
                             <Input
+                                className={styles.button_isactive}
                                 type="email"
                                 id="profile__email"
-                                value={user?.email}
+                                value={email}
+                                disabled={!editEmail}
+                                onChange={(e) => setEmail(e.target.value)}
                             />
-                            <Button size="small">ИЗМЕНИТЬ</Button>
+                            <Button
+                                className={styles.edit_button}
+                                type="button"
+                                size="small"
+                                onClick={emailEditHandler}
+                                style={{
+                                    backgroundColor: editEmail
+                                        ? 'var(--second-primary)'
+                                        : 'var(--accent)',
+                                }}
+                            >
+                                {editEmail ? 'ОТМЕНИТЬ' : 'ИЗМЕНИТЬ'}
+                            </Button>
                         </div>
                         <div className={styles.input}>
                             <label htmlFor="profile__password">
                                 Ваш пароль
                             </label>
                             <Input
+                                className={styles.button_isactive}
                                 type="password"
                                 id="profile__password"
                                 placeholder="***********"
-                                value={''}
+                                value={password}
+                                disabled={!editPassword}
+                                onChange={(e) => setPassword(e.target.value)}
                             />
-                            <Button size="small">ИЗМЕНИТЬ</Button>
+                            <Button
+                                className={styles.edit_button}
+                                type="button"
+                                size="small"
+                                onClick={passwordEditHandler}
+                                style={{
+                                    backgroundColor: editPassword
+                                        ? 'var(--second-primary)'
+                                        : 'var(--accent)',
+                                }}
+                            >
+                                {editPassword ? 'ОТМЕНИТЬ' : 'ИЗМЕНИТЬ'}
+                            </Button>
                         </div>
                         <div className={styles.input}>
                             <label htmlFor="profile__name">Ваше имя</label>
                             <Input
                                 type="text"
                                 id="profile__name"
-                                value={user?.username}
+                                value={username}
+                                onChange={(e) => setName(e.target.value)}
                             />
                             <Button
                                 style={{ opacity: 0 }}
@@ -79,6 +146,9 @@ export const Profile = () => {
                             </Button>
                         </div>
                     </div>
+                    <Button className={styles.save_changes} size="medium">
+                        СОХРАНИТЬ
+                    </Button>
                 </form>
             </div>
         </div>
